c932c7633bd355293f1bd9584d0d2194b7f9fc7d
Uli Ehler <git@uli.io>
2018-05-10 17:52:27 +0200

    removed tracking

--- js/all.js
+++ js/all.js
@@ -5,7 +5,7 @@ function init() {
 }
 
 function triggerImprint() {
-    var imprint = document.getElementById('imprint');
+    let imprint = document.getElementById('imprint');
 
     if (imprint.classList.contains('hidden')) {
         imprint.classList.remove('hidden');
@@ -13,23 +13,3 @@ function triggerImprint() {
         imprint.classList.add('hidden');
     }
 }
-
-/* piwik */
-var _paq = _paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-_paq.push(["setCookieDomain", "*.uli.io"]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function () {
-    var u = "https://piwik.ehlertainment.de/";
-    _paq.push(['setTrackerUrl', u + 'piwik.php']);
-    _paq.push(['setSiteId', '3']);
-    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.type = 'text/javascript';
-    g.async = true;
-    g.defer = true;
-    g.src = u + 'piwik.js';
-    s.parentNode.insertBefore(g, s);
-})();
-/* end piwik */
